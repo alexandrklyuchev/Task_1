82f58534773b8574ff93f6cec6a82048a3acd84d
Vishnenko Maxim <vishnenko.maxim@gmail.com>
2023-03-01 20:08:23 +0500

    second

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include <stdio.h>
+
+#include "version.h"
+
+int version() {
+    return PROJECT_VERSION_PATCH;
+}
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    printf("%d\n", version());
     return 0;
 }
